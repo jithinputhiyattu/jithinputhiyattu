a0f8b19d45513b82aefa6994380da26ab19b21c3
Puthiyattu <jithinputhiyattu@gmail.com>
2019-03-12 14:15:40 +0530

    [UI] Remove warnings

--- ui/src/components/header/Header.js
+++ ui/src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 class Header extends Component {
     render() {
--- ui/src/components/main/Main.js
+++ ui/src/components/main/Main.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Header from './../header/Header';
 import Resume from './../resume/Resume';
-import {  BrowserRouter , Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 export default class Main extends Component {
-  render() {
-    return (
-          <BrowserRouter>
-          <div>
-
-                <Header></Header>
-                <Route path="/resume" component={Resume} ></Route>
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header></Header>
+                    <Route path="/resume" component={Resume} ></Route>
                 </div>
-          </BrowserRouter>
-    )
-  }
+            </BrowserRouter>
+        )
+    }
 }
